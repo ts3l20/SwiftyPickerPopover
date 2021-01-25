3388a74674ca3b0df8a6aec12cce8007550e86d1
ts3l20 <y_tetsuo@hotmail.com>
2021-01-25 12:42:54 +0900

    fix bracket

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyPickerPopover",
-            dependencies: []),
+            dependencies: [],
              path: "SwiftyPickerPopover",
              exclude: ["Info.plist"],
              resources: [
